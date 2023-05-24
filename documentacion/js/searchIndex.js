8fd6f8f1ea72191b783aef0f58202fd76ccdc63f
Lauravdlm <134529514+Lauravdlm@users.noreply.github.com>
2023-05-24 18:31:55 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020la\u0020suma\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\resta\u0028\u0029",
+            "name": "resta",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020la\u0020resta\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_resta"
+        },                {
+            "fqsen": "\\saluda\u0028\u0029",
+            "name": "saluda",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020un\u0020saludo\u0020con\u0020el\u0020nombre\u0020que\u0020le\nenviamos\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_saluda"
+        },                {
+            "fqsen": "\\Ejemplo",
+            "name": "Ejemplo",
+            "summary": "Esto\u0020es\u0020un\u0020ejemplo\u0020de\u0020documentaci\u00F3n\u0020de\u0020PHP",
+            "url": "classes/Ejemplo.html"
+        },                {
+            "fqsen": "\\Ejemplo\u003A\u003Acheck_dir_email\u0028\u0029",
+            "name": "check_dir_email",
+            "summary": "Verifica\u0020si\u0020una\u0020direccion\u0020de\u0020correo\u0020es\u0020correcta\u0020o\u0020no.",
+            "url": "classes/Ejemplo.html#method_check_dir_email"
+        },                {
+            "fqsen": "\\Ejemplo\u003A\u003Aencuentra_html_imagenes\u0028\u0029",
+            "name": "encuentra_html_imagenes",
+            "summary": "Localiza\u0020las\u0020imagenes\u0020dentro\u0020del\u0020contenido",
+            "url": "classes/Ejemplo.html#method_encuentra_html_imagenes"
+        },                {
+            "fqsen": "\\Ejemplo\u003A\u003A\u0024to",
+            "name": "to",
+            "summary": "Array\u0020donde\u0020almacenaremos\u0020los\u0020destinatarios\u0020del\u0020mensaje",
+            "url": "classes/Ejemplo.html#property_to"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
